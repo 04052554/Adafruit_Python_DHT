6bcb43ccf24c3b55fb774b4332a79cf618beb5ae
04052554 <simon222star@gmail.com>
2017-12-06 08:53:21 +0000

    Check Point 3

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -20,8 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import Adafruit_DHT
-
-
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 import time
 import sys
 import httplib, urllib
@@ -71,12 +72,18 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # If this happens try again!
 while True:
 	h0, t0= Adafruit_DHT.read_retry(sensor, pin)
+	SwitchStatus = GPIO.input(24)
+	if(SwitchStatus == 0):
+		print('Button pressed')
+	else:
+		print('Button released')
         if humidity is not None and temperature is not None:
                 print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature,humidity))
 		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
-				{"dataChnId":"Temperature","values":{"value":t0}}]} 
+					 {"dataChnId":"Temperature","values":{"value":t0}},
+					 {"dataChnId":"SwitchStatus","values":{"value":SwitchStatus}}]} 
 		post_to_mcs(payload)
-		time.sleep(10) 
+		time.sleep(1) 
 
         else:
                 print('Failed to get reading. Try again!')
